de2a4c85c51c76e84eb66ba4714a86377eef30b6
bouzuya <m@bouzuya.net>
2025-05-23 22:32:35 +0900

    Read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::io::Read;
+
 mod tm1;
 mod tm2;
 mod tm3;
 
-fn main() {}
+fn main() {
+    let mut stdin = std::io::stdin();
+    let mut s = String::new();
+    stdin.read_to_string(&mut s).unwrap();
+    for token in s.split_whitespace() {
+        println!("{}", token);
+    }
+}
