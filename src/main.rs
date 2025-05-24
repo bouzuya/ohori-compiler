94b83efca5b952b4672b8029ea610e1a13477e78
bouzuya <m@bouzuya.net>
2025-05-24 22:53:50 +0900

    Add args support

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,16 @@ mod tm2;
 mod tm3;
 
 fn main() {
-    let mut stdin = std::io::stdin();
-    let mut s = String::new();
-    stdin.read_to_string(&mut s).unwrap();
-    for token in s.split_whitespace() {
-        println!("{}", token);
+    let args = std::env::args().collect::<Vec<String>>();
+    let mut read: Box<dyn std::io::Read> = match args.iter().nth(1) {
+        Some(a) => Box::new(std::fs::File::open(a).unwrap()),
+        None => Box::new(std::io::stdin()),
+    };
+    loop {
+        let mut s = String::new();
+        read.read_to_string(&mut s).unwrap();
+        for token in s.split_whitespace() {
+            println!("{}", token);
+        }
     }
 }
